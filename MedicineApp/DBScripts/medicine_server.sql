ab4d038f16cd15ffd3fbcfe12ec14624f18e2cff
User <User@LAB2-2>
2024-10-07 13:11:03 +0300

    e

--- MedicineApp/DBScripts/medicine_server.sql
+++ MedicineApp/DBScripts/medicine_server.sql
@@ -1,34 +1,40 @@
 CREATE DATABASE medicine_server
  CREATE TABLE Users (
- UserId int NOT NULL PRIMARY KEY,
+ UserId int NOT NULL IDENTITY(2,1) PRIMARY KEY,
  FirstName NVarchar (255),
  LastName NVarchar (255),
  UserName Varchar(30),
  UserPass Varchar (255),
  UserRank int CHECK(UserRank=1 OR UserRank = 2)
  );
- CREATE TABLE Orders(
- OrderId int NOT NULL PRIMARY KEY,
- FOREIGN KEY (FK_Medicine) REFERENCES Medicines(MedicineId),
- FOREIGN KEY (FK_Receiver) REFERENCES Users(UserId),
- Receiver Geography,
- Sender Geography,
- );
- CREATE TABLE Medicines(
-  MedicineId int NOT NULL PRIMARY KEY,
-  FOREIGN KEY (FK_PharmacyId) REFERENCES Pharmacies(PharmacyId),
-  MedicineName Varchar,
-  BrandName Nvarchar,
-  FOREIGN KEY (FK_StatusId) REFERENCES MedicineStatuses(StatusId),
- );
- CREATE TABLE MedicineStatuses(
- StatusId int NOT NULL PRIMARY KEY,
- MStatus Varchar CHECK(MStatus = "Approved" OR MStatus = "Denied" OR MStatus = "Checking"),
- Notes NVarchar,
- );
  CREATE TABLE Pharmacies (
- PharmacyId int NOT NULL PRIMARY KEY,
+ PharmacyId int NOT NULL IDENTITY(1,1) PRIMARY KEY,
  PharmacyName NVarchar(255),
  Adress Geography,
  Phone int,
  );
+ CREATE TABLE Medicines(
+  MedicineId int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+  PharmacyId int
+  FOREIGN KEY (PharmacyId) REFERENCES Pharmacies(PharmacyId),
+  MedicineName Varchar,
+  BrandName Nvarchar,
+  StatusId int
+  FOREIGN KEY (StatusId) REFERENCES MedicineStatuses(StatusId),
+ );
+ CREATE TABLE Orders(
+ OrderId int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+ MedicineId int
+ FOREIGN KEY (MedicineId) REFERENCES Medicines(MedicineId),
+ UserId int
+ FOREIGN KEY (UserId) REFERENCES Users(UserId),
+ Receiver Geography,
+ Sender Geography,
+ );
+ CREATE TABLE MedicineStatuses(
+ StatusId int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+ MStatus Varchar, CHECK(MStatus = 'Approved' OR MStatus = 'Denied' OR MStatus = 'Checking' OR Mstatus=NULL),
+ Notes NVarchar,
+ );
+ 
+ 

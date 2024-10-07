e26506d1cb07a15615388dc9d28152af0345fccb
User <User@LAB2-2>
2024-10-07 12:47:02 +0300

    fixed sql

--- MedicineApp/DBScripts/medicine_server.sql
+++ MedicineApp/DBScripts/medicine_server.sql
@@ -3,22 +3,32 @@ CREATE DATABASE medicine_server
  UserId int NOT NULL PRIMARY KEY,
  FirstName NVarchar (255),
  LastName NVarchar (255),
- IsAdmin bit,
+ UserName Varchar(30),
+ UserPass Varchar (255),
+ UserRank int CHECK(UserRank=1 OR UserRank = 2)
  );
  CREATE TABLE Orders(
  OrderId int NOT NULL PRIMARY KEY,
-
+ FOREIGN KEY (FK_Medicine) REFERENCES Medicines(MedicineId),
+ FOREIGN KEY (FK_Receiver) REFERENCES Users(UserId),
+ Receiver Geography,
+ Sender Geography,
  );
  CREATE TABLE Medicines(
   MedicineId int NOT NULL PRIMARY KEY,
-  FOREIGN KEY (FK_PharmacyId) REFERENCES Pharmacies(PharmacyId) 
+  FOREIGN KEY (FK_PharmacyId) REFERENCES Pharmacies(PharmacyId),
+  MedicineName Varchar,
+  BrandName Nvarchar,
+  FOREIGN KEY (FK_StatusId) REFERENCES MedicineStatuses(StatusId),
  );
  CREATE TABLE MedicineStatuses(
- FOREIGN KEY (MedicineId) REFERENCES Medicines(MedicineId),
+ StatusId int NOT NULL PRIMARY KEY,
+ MStatus Varchar CHECK(MStatus = "Approved" OR MStatus = "Denied" OR MStatus = "Checking"),
+ Notes NVarchar,
  );
  CREATE TABLE Pharmacies (
  PharmacyId int NOT NULL PRIMARY KEY,
  PharmacyName NVarchar(255),
  Adress Geography,
-
+ Phone int,
  );
